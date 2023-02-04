48e28987897ef0c067a0fb0e7f950b683268327c
bowwow2115 <bowwow2115@gmail.com>
2023-02-04 22:54:25 +0900

    Iterator Pattern

---
+++ Iterator/MainEntry.java
@@ -0,0 +1,20 @@
+package Iterator;
+
+public class MainEntry {
+    public static void main(String[] args) {
+        Item[] items = {
+                new Item("CPU", 1000),
+                new Item("Keyboard", 100),
+                new Item("Mouse", 50),
+                new Item("HDD", 10)
+        };
+
+        Array array = new Array(items);
+        Iterator it = array.iterator();
+
+        while (it.next()) {
+            Item item = (Item) it.current();
+            System.out.println(item);
+        }
+    }
+}
