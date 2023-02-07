08b67daa0bfc3c39875a2df4bff6a796b01aa365
bowwow2115 <bowwow2115@gmail.com>
2023-02-07 23:58:39 +0900

    flyweight 패턴

---
+++ flyweight/MainEntry.java
@@ -0,0 +1,12 @@
+package flyweight;
+
+public class MainEntry {
+    public static void main(String[] args) {
+        Number number = new Number(133113);
+        number.print(5, 5);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
