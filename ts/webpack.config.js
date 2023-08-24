3ad5e0b3aaf4a58aa3f68692cff111a236701667
SeungHunPark <bowwow94@inzent.com>
2023-08-24 17:43:29 +0900

    Observer 패턴 추가

--- ts/webpack.config.js
+++ ts/webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   // entry: "./src/singleton/index.ts",
   // entry: "./src/flyweight/index.ts",
   // entry: "./src/composite/index.ts",
-  entry: "./src/factory_method/index.ts",
+  // entry: "./src/factory_method/index.ts",
+  entry: "./src/observer/index.ts",
   devtool: "inline-source-map",
   module: {
     rules: [
